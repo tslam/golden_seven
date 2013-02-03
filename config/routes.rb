c27be314e6f264fa05be18e183a1e1806c88f1a8
Raghu Betina <rbetina@gmail.com>
2013-02-03 16:56:44 -0600

    Let's name our routes according to which resource the URL is locating.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 GoldenSeven::Application.routes.draw do
   root :to => 'Pictures#index'
 
-  get '/pictures/new' => 'Pictures#new'
-  post '/pictures' => 'Pictures#create'
+  get '/pictures/new' => 'Pictures#new', :as => 'new_picture'
+  post '/pictures' => 'Pictures#create', :as => 'pictures'
 
-  get '/pictures' => 'Pictures#index'
-  get '/pictures/:id' => 'Pictures#show'
+  get '/pictures' => 'Pictures#index', :as => 'pictures'
+  get '/pictures/:id' => 'Pictures#show', :as => 'picture'
 
-  get '/pictures/:id/edit' => 'Pictures#edit'
-  put '/pictures/:id' => 'Pictures#update'
+  get '/pictures/:id/edit' => 'Pictures#edit', :as => 'edit_picture'
+  put '/pictures/:id' => 'Pictures#update', :as => 'picture'
 
-  delete '/pictures/:id' => 'Pictures#destroy'
+  delete '/pictures/:id' => 'Pictures#destroy', :as => 'picture'
 end
