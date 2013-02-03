a28b9f92bbd12aec3381b219074d93abb0c91804
Raghu Betina <rbetina@gmail.com>
2013-02-03 17:18:21 -0600

    Since form_for, unlike form_tag, wraps up all the form data in a sub-hash within the params hash, we need to modify the logic in the create method to drill one extra level down.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -13,8 +13,8 @@ class PicturesController < ApplicationController
 
   def create
     picture = Picture.new
-    picture.source = params[:source]
-    picture.caption = params[:caption]
+    picture.source = params[:picture][:source]
+    picture.caption = params[:picture][:caption]
     picture.save
 
     redirect_to pictures_url
