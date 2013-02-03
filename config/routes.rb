57409c2e68b041f4d836da49feaa4595e802b3bf
Raghu Betina <rbetina@gmail.com>
2013-02-03 15:11:53 -0600

    Next up: the user should be able to request a form to enter information for a new picture.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 GoldenSeven::Application.routes.draw do
+  get '/pictures/new', :controller => 'Pictures', :action => 'new'
+
   get '/pictures', :controller => 'Pictures', :action => 'index'
   get '/pictures/:id', :controller => 'Pictures', :action => 'show'
 end
