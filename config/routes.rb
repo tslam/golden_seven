84eee720d4e1ec2246c3ce145e39aed653a5467d
Raghu Betina <rbetina@gmail.com>
2013-02-03 15:29:35 -0600

    Next up: the user should be able to request a form to enter information to edit an existing picture.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ GoldenSeven::Application.routes.draw do
 
   get '/pictures', :controller => 'Pictures', :action => 'index'
   get '/pictures/:id', :controller => 'Pictures', :action => 'show'
+
+  get 'pictures/:id/edit', :controller => 'Pictures', :action => 'edit'
 end
