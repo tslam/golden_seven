0315a96779007afd838a5b00d102237152eeacfc
Raghu Betina <rbetina@gmail.com>
2013-02-03 15:21:05 -0600

    Next up: when the user clicks submit, we need receive the information from the form and create a new row in the database.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 GoldenSeven::Application.routes.draw do
   get '/pictures/new', :controller => 'Pictures', :action => 'new'
+  post '/pictures', :controller => 'Pictures', :action => 'create'
 
   get '/pictures', :controller => 'Pictures', :action => 'index'
   get '/pictures/:id', :controller => 'Pictures', :action => 'show'
